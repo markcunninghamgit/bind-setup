79aeb6d8a703f74443fb3ea61c37f6f4f0b4ccef
user <user@desktop>
2013-01-17 01:25:35 +0000

    first commit

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+apt-get -y install bind9
+
+if [ ! -e /etc/bind/named.conf.log ]; then
+        cp named.conf.log /etc/bind/named.conf.log
+else
+        echo "named.conf.log already exists. overwrite? (y/n)"
+        read overwrite
+        if [ "$overwrite" = "y" ]; then
+                cp named.conf.log /etc/bind/named.conf.log
+        fi
+fi
+exit
+
+mkdir /etc/bind/zones
+chown -R bind:bind /etc/bind/zones
+mkdir /var/log/bind
+cd /var/log/bind
+
+touch bind.log queries.log security_info.log update_debug.log
+chown -R bind:bind /var/log/bind
+
+if ! grep -q named.conf.log /etc/bind/named.conf; then
+        echo 'include "/etc/bind/named.conf.log";' >> /etc/bind/named.conf
+else
+        echo "named.conf.log already in named.conf"
+fi
